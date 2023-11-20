169e70ad2302b49bf26959143b4f64c1fb0f5d66
MaxximusJackson <111911331+MaxximusJackson@users.noreply.github.com>
2023-11-20 11:30:08 -0600

    save

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,119 +1,172 @@
 import express from 'express';
 import debug from 'debug';
 const debugBook = debug('app:Book');
-import { connect,getBooks,getBookById, updateBook, addBook, deleteBook, saveEdit } from '../../database.js';
-import {validId} from '../../middleware/validId.js';
-import { validBody } from '../../middleware/validBody.js';
+import { connect,getBooks, getBookById, updateBook, addBook, deleteBook, saveEdit } from '../../database.js';
+import { validId } from '../../middleware/validId.js';
+import {validBody} from '../../middleware/validBody.js';
 import Joi from 'joi';
-import {isLoggedIn} from '@merlin4/express-auth';
+import { isLoggedIn, hasPermission } from '@merlin4/express-auth';
 
 const router = express.Router();
 
 const newBookSchema = Joi.object({
-  isbn:Joi.string().trim().min(14).required(),
-  title:Joi.string().trim().min(1).required(),
-  author:Joi.string().trim().min(1).required(),
-  genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction').required(),
-  publication_year:Joi.number().integer().min(1900).max(2023).required(),
-  price:Joi.number().min(0).required(),
-  description:Joi.string().trim().min(1).required(),
+    isbn:Joi.string().trim().min(14).required(),
+    title:Joi.string().trim().min(1).required(),
+    author:Joi.string().trim().min(1).required(),
+    genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction').required(),
+    publication_year:Joi.number().integer().min(1900).max(2023).required(),
+    price:Joi.number().min(0).required(),
+    description:Joi.string().trim().min(1).required(),
 });
 
 const updateBookSchema = Joi.object({
-  isbn:Joi.string().trim().min(14),
-  title:Joi.string().trim().min(1),
-  author:Joi.string().trim().min(1),
-  genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction', 'Adventure'),
-  publication_year:Joi.number().integer().min(1900).max(2023),
-  price:Joi.number().min(0),
-  description:Joi.string().trim().min(1),
-})
+    isbn:Joi.string().trim().min(14),
+    title:Joi.string().trim().min(1),
+    author:Joi.string().trim().min(1),
+    genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction'),
+    publication_year:Joi.number().integer().min(1900).max(2023),
+    price:Joi.number().min(0),
+    description:Joi.string().trim().min(1),
+});
 
-router.get('/list', isLoggedIn(), async (req, res) => {
+
+router.get('/list', isLoggedIn(), hasPermission('canListBooks'), async (req, res) => {
+
+    debugBook(`The req.auth property is: ${JSON.stringify(req.auth)}`);
  
-  try {
-    const db = await getBooks();
-    const books = await getBooks();
-  res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({error: error.stack});
-  }
+    debugBook(`The req.auth property is: ${JSON.stringify(req.auth)}`);
+    let {keywords, minPrice,maxPrice, genre, sortBy, pageSize, pageNumber} = req.query;
+    const match = {}; 
+    let sort = {author:1};
+
+    try
+    {
+
+        if(keywords){
+            match.$text = {$search: keywords};
+        }
+
+        if(genre){
+            match.genre = {$eq: genre};
+        }
+
+        if(minPrice && maxPrice){
+            match.price = {$gte: parseFloat(minPrice), $lte: parseFloat(maxPrice)};
+        }else if(minPrice){
+            match.price = {$gte: parseFloat(minPrice)};
+        }else if(maxPrice){
+            match.price = {$lte: parseFloat(maxPrice)};
+        }
+
+        switch(sortBy){
+            case "price": sort = {price : 1}; break;
+            case "year" : sort = {publication_year: 1}; break;
+        }
+
+ 
+
+        pageNumber = parseInt(pageNumber) || 1;
+        pageSize = parseInt(pageSize) || 100;
+        const skip = (pageNumber - 1) * pageSize;
+        const limit = pageSize;
+        debugBook(`Skip is ${skip} and limit is ${limit}`);
+        const pipeline = [
+            {$match: match},
+            {$sort: sort},
+            {$skip: skip},
+            {$limit: limit}
+        ];
+
+        const db = await connect();
+        const cursor = await db.collection('Book').aggregate(pipeline);
+        const books = await cursor.toArray();
+        res.status(200).json(books);
+
+    } catch(err){
+        res.status(500).json({error: err.stack});
+    }
+
+  
+});
+
+
+router.get('/:id', isLoggedIn(), validId('id'), async (req,res) => {
+    const id = req.id;
+    try{
+        const book = await getBookById(id);
+        if(book){
+            res.status(200).json(book);
+        }else{
+            res.status(404).json({message: `Book ${id} not found`});
+        }
+    } catch(err){
+        res.status(500).json({error: err.stack});
+    }
+});
+
+
+router.put('/update/:id', isLoggedIn(), validId('id'), validBody(updateBookSchema), async (req,res) => {
+    const id = req.id;
+    const updatedBook = req.body;
+    if(updatedBook.price){
+        updatedBook.price = parseFloat(updatedBook.price);
+    }
+   try{
+        const updateResult = await updateBook(id, updatedBook);
+        debugBook(`Update result is ${JSON.stringify(updateResult)}`);
+        if(updateResult.modifiedCount == 1){
+            const edit ={
+                timeStamp: new Date(),
+                op:'Update Book',
+                collection:'Book',
+                target:id,
+                auth:req.auth
+            }
+            await saveEdit(edit);
+            res.status(200).json({message: `Book ${id} updated`});
+        }else{
+            res.status(400).json({message: `Book ${id} not updated`});
+        }
+    }catch(err){
+        res.status(500).json({error: err.stack});
+    }
 });
 
 
 
-router.get('/:id', isLoggedIn(), validId('id'), async (req, res) => {
-  const id = req.id;
-  try {
-    const book = await getBookById(id);
-    if(book){
-      res.status(200).json(book);
-    }else{
-      res.status(404).json({message: `Book ${id} not found`});
-    }
+router.post('/add', isLoggedIn(), validBody(newBookSchema), async (req,res) => {
+
+    const newBook = req.body;
    
-  } catch (error) {
-    res.status(500).json({error: error.stack});
-  }
-});
-
-router.put('/update/:id', isLoggedIn(), validId('id'), validBody(updateBookSchema), async (req, res) => {
-  const id = req.id;
-  const updatedBook = req.body;
-
-  if(updatedBook.price){
-    updatedBook.price = parseFloat(updatedBook.price);
-  }
-  try {
-    const updateResult = await updateBook(id, updatedBook);
-    if (updateResult.modifiedCount == 1) {
-      const edit = {
-        timeStamp: new Date(),
-        op: 'Update Book',
-        collection:'Book',
-        target:id,
-        auth:req.auth
-      }
-      await saveEdit(edit);
-      res.status(200).json({message: `Book ${id} updated`});
-    } else {
-      res.status(400).json({message: `Book ${id} not updated`});
+    try{
+        const dbResult = await addBook(newBook);
+        if(dbResult.acknowledged == true){
+            res.status(200).json({message: `Book ${newBook.title} added with an id of ${dbResult.insertedId}`});
+        }else{
+            res.status(400).json({message: `Book ${newBook.title} not added`});
+        }
+    } catch(err){
+     res.status(500).json({error: err.stack});
     }
-  } catch (err) {
-    res.status(500).json({error: err.stack});
-  }
-  
 });
 
-router.post('/add', isLoggedIn(), validBody(newBookSchema), async (req, res) => {
-  const newBook = req.body;
-  const dbResult = await addBook(newBook);
-  try{
-  if(dbResult.acknowledged == true){
-    res.status(200).json({message: `Book ${newBook.title} added`});
-  }else{
-    res.status(400).json({message: `Book ${newBook.title} not added`});
-  }
-}catch(err){
-  res.status(500).json({error: err.stack});
-}
-});
 
-router.delete('/delete/:id', isLoggedIn(), validId('bookId'), async(req, res) => {
-  const id = req.id;
-  
-  try {
-    const dbResult = await deleteBook(id);
+router.delete('/delete/:bookId', isLoggedIn(), hasPermission('canDeleteBook'), validId('bookId'), async (req,res) => {
 
-    if (dbResult.deletedCount == 1) {
-      res.status(200).json({message: `Book ${id} deleted`});
-    }else{
-      res.status(400).json({message: `Book ${id} not deleted`});
+    const id = req.bookId;
+
+    try{
+        const dbResult = await deleteBook(id);
+
+        if(dbResult.deletedCount == 1){
+            res.status(200).json({message: `Book ${id} deleted`});
+        }else{
+            res.status(400).json({message: `Book ${id} not deleted`});
+        }
+    }catch(err){
+        res.status(500).json({error: err.stack});
     }
-  } catch (err) {
-    res.status(500).json({error: err.stack});
-  }
-  
+
 });
 
 export {router as BookRouter};
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,182 +2,187 @@ import express from 'express';
 import debug from 'debug';
 const debugUser = debug('app:User');
 debugUser.color = '63';
-import {findRoleByName, addUser, loginUser, newId, getAllUsers, getUserById, updateUser, saveEdit} from '../../database.js';
+import {findRoleByName,addUser, loginUser, newId,getAllUsers,getUserById, updateUser, saveEdit} from '../../database.js';
 import bcrypt from 'bcrypt';
-import {validBody} from '../../middleware/validBody.js';
+import { validBody } from '../../middleware/validBody.js';
 import Joi from 'joi';
 import jwt from 'jsonwebtoken';
 import { validId } from '../../middleware/validId.js';
-import { isLoggedIn, fetchRoles, mergePermissions, hasPermission} from '@merlin4/express-auth';
-import { Timestamp } from 'mongodb';
-
+import { isLoggedIn, fetchRoles, mergePermissions, hasPermission } from '@merlin4/express-auth';
 
 const router = express.Router();
 
 async function issueAuthToken(user){
-  const payload = {_id: user._id, email: user.email, role: user.role};
-  const secret = process.env.JWT_SECRET;
-  const options = {expiresIn:'1h'};
+    const payload = {_id: user._id, email: user.email, role: user.role};
+    const secret = process.env.JWT_SECRET;
+    const options = {expiresIn:'1h'};
 
-  const roles = await fetchRoles(user, role => findRoleByName(role));
-  // roles.forEach(role => {
-  //   debugUser(`The users roles are ${(role)} and has the following permissions: ${JSON.stringify(role.permissions)}`);
-  // });
+
+    const roles = await fetchRoles(user, role => findRoleByName(role));
+
   
-  const permissions = mergePermissions(user, roles);
-  payload.permissions = permissions;
+    const permissions = mergePermissions(user, roles);
+    payload.permissions = permissions;
 
-  const authToken = jwt.sign(payload, secret, options);
-  return authToken;
+
+
+    const authToken = jwt.sign(payload, secret, options);
+    return authToken;
 }
 
 function issueAuthCookie(res, authToken){
-  const cookieOptions = {httpOnly:true,maxAge:1000*60*60};
-  res.cookie('authToken',authToken, cookieOptions);
+    const cookieOptions = {httpOnly:true,maxAge:1000*60*60};
+    res.cookie('authToken',authToken,cookieOptions);
 }
 
+
 const newUserSchema = Joi.object({
-  fullName:Joi.string().trim().min(1).max(50).required(),
-  password:Joi.string().trim().min(8).max(50).required(),
-  email:Joi.string().trim().email().required()
+    fullName:Joi.string().trim().min(1).max(50).required(),
+    password:Joi.string().trim().min(8).max(50).required(),
+    email:Joi.string().trim().email().required(),
 });
 
 const loginUserSchema = Joi.object({
-  email:Joi.string().trim().email().required(),
-  password:Joi.string().trim().min(8).max(50).required()
+    email:Joi.string().trim().email().required(),
+    password:Joi.string().trim().min(8).max(50).required()
 });
 
 const updateUserSchema = Joi.object({
-  fullName:Joi.string().trim().min(1).max(50),
-  password:Joi.string().trim().min(8).max(50)
+    fullName:Joi.string().trim().min(1).max(50),
+    password:Joi.string().trim().min(8).max(50),
 });
 
-router.get('/list', isLoggedIn(), hasPermission('canListBooks'), async(req,res) => {
-  debugUser('Getting all users');
-  try{
-    let users = await getAllUsers();
-    res.status(200).json(users);
-  }catch(err){
-    res.status(500).json({error: err.stack});
-  }
-  
+router.get('/list', isLoggedIn(), async (req, res) => {
+    debugUser('Getting all users');
+    try{
+        let users = await getAllUsers();
+
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json({error: err.stack});
+    }
 });
 
-router.post('/add', validBody(newUserSchema), async (req, res) => {
-  const newUser = {
-    _id: newId(),
-    ...req.body,
-    createdDate: new Date(),
-  }
-  newUser.password = await bcrypt.hash(newUser.password, 10);
-  try {
-    const result = await addUser(newUser);
-    if(result.acknowledged==true){
-      const authToken = await issueAuthToken(newUser);
-      issueAuthCookie(res, authToken);
-
-      res.status(200).json({message: `User ${result.insertedId} added. Your auth token is ${authToken}`});
+router.post('/add', validBody(newUserSchema), async (req,res) => {
+    const newUser = 
+    {
+        _id: newId(),
+        ...req.body,
+        createdDate: new Date(),
     }
     
-  } catch (err) {
-    res.status(500).json({error: err.stack});
-  }
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    try{
+        const result = await addUser(newUser);
+       if(result.acknowledged==true){
+
+            const authToken = await issueAuthToken(newUser);
+            issueAuthCookie(res, authToken);
+
+            res.status(200).json({message: `User ${result.insertedId} added.  Your auth token is ${authToken}`});
+       }
+    }catch(err){
+        res.status(500).json({error: err.stack});
+    }
 });
 
-router.post('/login', validBody(loginUserSchema), async (req, res) => {
-  const user = req.body;
+router.post('/login', validBody(loginUserSchema), async (req,res) => {
+    const user = req.body;
 
-  const resultUser = await loginUser(user);
-  debugUser(resultUser);
-  if (resultUser && await bcrypt.compare(user.password, resultUser.password)) {
-    const authToken = await issueAuthToken(resultUser);
-    issueAuthCookie(res, authToken);
-    res.status(200).json({
-      message: `Welcome ${resultUser.fullName}`,
-      authToken:authToken,
-      email:resultUser.email,
-      fullName:resultUser.fullName,
-    });
-  }else{
-    res.status(400).json(`email or password incorrect`);
-  }
+    const resultUser = await loginUser(user);
+    debugUser(resultUser);
+    if(resultUser && await bcrypt.compare(user.password, resultUser.password)){
+        const authToken = await issueAuthToken(resultUser);
+        issueAuthCookie(res, authToken);
+        res.status(200).json({
+            message:`Welcome ${resultUser.fullName}`,
+            authToken:authToken,
+            email:resultUser.email,
+            fullName:resultUser.fullName,
+        } );
+    }else{
+        res.status(401).json(`email or password incorrect`);
+    }
 });
 
 router.post('/logout', isLoggedIn(), async (req,res) => {
-  res.clearCookie('authToken');
-  res.status(200).json({message:'Logged out'});
+    res.clearCookie('authToken');
+    res.status(200).json({message:'Logged Out'});
 });
 
-router.put('/update/me', isLoggedIn(), validBody(updateUserSchema), async (req,res) => {
-  debugUser(`Self Service Route Updating a user ${JSON.stringify(req.auth)}`);
-  const updatedUser = req.body;
-  
-  try {
-    const user = await getUserById(newId(req.auth._id));
-    if(user){
-      if(updatedUser.fullName){
-        user.fullName = updatedUser.fullName;
-      }
-      if(updatedUser.password){
-        user.password = await bcrypt.hash(updatedUser.password, 10);
-      }
-      const dbResult = await updateUser(user);
-      if(dbResult.modifiedCount == 1){
-        const edit = {
-          timeStamp: new Date(),
-          op: 'Self-Edit Update User',
-          collection:'User',
-          target:user._id,
-          auth:req.auth
+
+router.put('/update/me', isLoggedIn(),validBody(updateUserSchema), async (req,res) => {
+    debugUser(`Self Service Route Updating a user ${JSON.stringify(req.auth)}`);
+    const updatedUser = req.body;
+   
+    try{
+        const user = await getUserById(newId(req.auth._id));
+        if(user){
+            if(updatedUser.fullName){
+                user.fullName = updatedUser.fullName;
+            }
+            if(updatedUser.password){
+                user.password = await bcrypt.hash(updatedUser.password, 10);
+            }
+            const dbResult = await updateUser(user);
+            if(dbResult.modifiedCount == 1){
+                const edit ={
+                    timeStamp: new Date(),
+                    op:'Self-Edit Update User',
+                    collection:'User',
+                    target:user._id,
+                    auth:req.auth
+                }
+                await saveEdit(edit);
+                res.status(200).json({message: `User ${req.auth._id} updated`});
+                return;
+            }else{
+                res.status(400).json({message: `User ${req.auth._id} not updated`});
+                return;
+            }
+        }else{
+            res.status(400).json({message: `User ${req.auth._id} not updated`});
         }
-        await saveEdit(edit);
-        res.status(200).json({message: `User ${req.id} updated`});
-        return;
-      } else {
-        res.status(400).json({message: `User ${req.id} not updated`});
-        return;
-      }
-    } else {
-      res.status(400).json({message: `User ${req.id} not updated`});
+    }catch(err){
+        res.status(500).json({error: err.stack});
     }
-  } catch (err) {
-    res.status(500).json({error: err.stack});
-  }
+   
 });
+
+
 
 router.put('/update/:id', isLoggedIn(), validId('id'), validBody(updateUserSchema), async (req,res) => {
-  debugUser('Admin Route Updating a user');
-  const updatedUser = req.body;
-  const user = await getUserById(req.id);
-  if(user){
-    if(updatedUser.fullName){
-      user.fullName = updatedUser.fullName;
-    }
-    if (updatedUser.password) {
-      user.password = await bcrypt.hash(updatedUser.password, 10);
-    }
-    const dbResult = await updateUser(user);
-    if(dbResult.modifiedCount == 1){
-      const edit = {
-        timeStamp: new Date(),
-        op: 'Admin Update User',
-        collection:'User',
-        target:user._id,
-        auth:req.auth
-      }
-      await saveEdit(edit);
-      res.status(200).json({message: `User ${req.id} updated`});
-      return;
-    }else {
-      res.status(400).json({message: `User ${req.id} not updated`});
-      return;
-    }
-  } else {
-    res.status(400).json({message: `User ${req.id} not updated`});
-  }
-  
-});
+    debugUser('Admin Route Updating a user');
+    const updatedUser = req.body;
+    const user = await getUserById(req.id);
 
+    if(user){
+        if(updatedUser.fullName){
+            user.fullName = updatedUser.fullName;
+        }
+        if(updatedUser.password){
+            user.password = await bcrypt.hash(updatedUser.password, 10);
+        }
+        const dbResult = await updateUser(user);
+        if(dbResult.modifiedCount == 1){
+            const edit ={
+                timeStamp: new Date(),
+                op:'Admin Update User',
+                collection:'User',
+                target:user._id,
+                auth:req.auth
+            }
+            await saveEdit(edit);
+            res.status(200).json({message: `User ${req.id} updated`});
+            return;
+        }else{
+            res.status(400).json({message: `User ${req.id} not updated`});
+            return;
+        }
+    }else{
+        res.status(400).json({message: `User ${req.id} not updated`});
+    }
+});
 
 
 export {router as UserRouter}
